232d2559365c23d28a77197dd5065b2b5e7a5ee1
James Tease <jamestease@gmail.com>
2015-02-11 16:09:45 +0000

    My god, it might actually be working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home8' });
+  res.render('index', { title: 'Test express site' });
 });
 
 module.exports = router;
