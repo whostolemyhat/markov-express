6a20acfd1b1a658c8cfc85b47a9e7815500ff333
James Tease <jamestease@gmail.com>
2015-02-11 15:03:49 +0000

    Testing forever again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home2' });
+  res.render('index', { title: 'Home3' });
 });
 
 module.exports = router;
